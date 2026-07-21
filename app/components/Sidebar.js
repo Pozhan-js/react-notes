f6d754180c8db5ae1e28e9ed8d6376647b9348d9
hashMi <854059946@qq.com>
2026-07-21 14:54:47 +0800

    feat: 首页基本布局

---
+++ app/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Sidebar() {
+  return (
+    <>
+      <section className="col sidebar">
+        <Link href={"/"} className="link--unstyled">
+          <section className="sidebar-header">
+            <Image
+              className="logo"
+              src="/logo.svg"
+              width={22}
+              height={20}
+              alt=""
+              role="presentation"
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className="sidebar-menu" role="menubar">
+          {/* SideSearchField */}
+        </section>
+        <nav>{/* SidebarNoteList */}</nav>
+      </section>
+    </>
+  );
+}
